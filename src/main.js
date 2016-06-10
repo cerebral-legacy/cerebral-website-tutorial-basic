28d987a5f56b7af5fb1a8b27cbdfb7e0d844adbf
Christian Alfoni <christianalfoni@gmail.com>
2016-06-10 23:24:38 +0200

    added main file

--- src/main.js
+++ src/main.js
@@ -1,1 +1,12 @@
-var test = "hest"
+import React from 'react'
+import {render} from 'react-dom'
+import {Container} from 'cerebral-view-react'
+
+import controller from './controller'
+import App from './components/App'
+
+render((
+  <Container controller={controller}>
+    <App />
+  </Container>
+), document.querySelector('#app'))
