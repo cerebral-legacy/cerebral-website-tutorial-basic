9895f0589e94034711f3362d1c43e323427a688d
Christian Alfoni <christianalfoni@gmail.com>
2016-06-10 22:41:25 +0200

    updated controller

--- src/controller.js
+++ src/controller.js
@@ -1,6 +1,17 @@
 import Controller from 'cerebral';
 import model from './model';
 
+import newItemTitleChanged from './signals/newItemTitleChanged';
+import newItemSubmitted from './signals/newItemSubmitted';
+
 const controller = Controller(model);
+
+controller.addSignals({
+  newItemTitleChanged: {
+    chain: newItemTitleChanged,
+    immediate: true
+  },
+  newItemSubmitted
+});
 
 export default controller;
