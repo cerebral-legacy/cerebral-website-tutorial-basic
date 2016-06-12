2dd2e9fea395e076d32f7ae636d9485f4d491bdb
Christian Alfoni <christianalfoni@gmail.com>
2016-06-12 15:05:10 +0200

    Added devtools

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Container} from 'cerebral-view-react'
+import Devtools from 'cerebral-module-devtools'
 
 import controller from './controller'
 import App from './components/App'
+
+controller.addModules({
+  devtools: Devtools()
+})
 
 render((
   <Container controller={controller}>
