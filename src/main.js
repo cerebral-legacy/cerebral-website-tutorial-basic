526edb35bac2ef7736f3d20d6ff565263ffecbf9
Christian Alfoni <christianalfoni@gmail.com>
2016-06-12 15:05:31 +0200

    Added devtools

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import {render} from 'cerebral-view-snabbdom'
+import Devtools from 'cerebral-module-devtools'
 
 import controller from './controller'
 import App from './components/App'
+
+controller.addModules({
+  devtools: Devtools()
+})
 
 render(() => App(), document.querySelector('#app'), controller)
