4d88639ad2a4dc578ebaf597bd4961a5f110f70e
Christian Alfoni <christianalfoni@gmail.com>
2016-06-15 14:09:10 +0200

    Changed to unshift

--- src/actions/addItem.js
+++ src/actions/addItem.js
@@ -1,8 +1,8 @@
 // This action grabs the current "newItemTitle"
-// from the state store and pushes it into the
-// "items" array
+// from the state store and putting it at the top of
+// the "items" array
 function addItem({state}) {
-  state.push('items', state.get('newItemTitle'))
+  state.unshift('items', state.get('newItemTitle'))
 }
 
 export default addItem
