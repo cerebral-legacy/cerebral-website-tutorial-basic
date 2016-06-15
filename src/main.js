17a0b0242aa48cfecdfe8dfaa7b068d9bf5a2b50
Christian Alfoni <christianalfoni@gmail.com>
2016-06-15 14:00:32 +0200

    added webpack config

--- src/main.js
+++ src/main.js
@@ -1,21 +1,15 @@
 import React from 'react'
 import {render} from 'react-dom'
 import {Container} from 'cerebral-view-react'
-<<<<<<< HEAD
 import Devtools from 'cerebral-module-devtools'
-=======
->>>>>>> master
 
 import controller from './controller'
 import App from './components/App'
 
-<<<<<<< HEAD
 controller.addModules({
   devtools: Devtools()
 })
 
-=======
->>>>>>> master
 render((
   <Container controller={controller}>
     <App />
