bfc991b9445f3d9c25d5d61e11373b3bef9a1be8
Christian Alfoni <christianalfoni@gmail.com>
2016-07-06 09:27:03 +0200

    Updated signature

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,37 +1,41 @@
 import React from 'react'
 import {connect} from 'cerebral-view-react'
 
-export default connect({
-  newItemTitle: 'newItemTitle',
-  items: 'items'
-}, function App (props) {
-  function onFormSubmit (event) {
-    event.preventDefault()
-    props.signals.newItemTitleSubmitted()
-  }
+export default connect(
+  {
+    newItemTitle: 'newItemTitle',
+    items: 'items'
+  },
+  function App(props) {
 
-  function onInputChange (event) {
-    props.signals.newItemTitleChanged({
-      title: event.target.value
-    })
-  }
+    const onFormSubmit = event => {
+      event.preventDefault()
+      props.signals.newItemTitleSubmitted()
+    }
 
-  return (
-    <div>
-      <form onSubmit={onFormSubmit}>
-        <input
-          type='text'
-          value={props.newItemTitle}
-          onChange={onInputChange}
-        />
-      </form>
-      <ul>
-        {props.items.map((item, index) => (
-          <li key={index}>
-            {item}
-          </li>
-        ))}
-      </ul>
-    </div>
-  )
-})
+    const onInputChange = event => {
+      props.signals.newItemTitleChanged({
+        title: event.target.value
+      })
+    }
+
+    return (
+      <div>
+        <form onSubmit={onFormSubmit}>
+          <input
+            type="text"
+            value={props.newItemTitle}
+            onChange={onInputChange}
+          />
+        </form>
+        <ul>
+          {props.items.map((item, index) => (
+            <li key={index}>
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
+)
